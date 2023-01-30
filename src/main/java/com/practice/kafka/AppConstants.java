21909017330663cff9764f6ca01db903b0ebf501
bahadur <bahadur@revechat.com>
2023-01-30 23:13:46 +0600

    Kafka Producer consumer completed.

---
+++ src/main/java/com/practice/kafka/AppConstants.java
@@ -0,0 +1,6 @@
+package com.practice.kafka;
+
+public interface AppConstants {
+    String TOPIC_NAME = "kafka_topic";
+    String GROUP_ID = "group_id";
+}
---
+++ src/main/java/com/practice/kafka/controller/KafkaProducerController.java
@@ -0,0 +1,22 @@
+package com.practice.kafka.controller;
+
+import com.practice.kafka.producer.KafkaProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class KafkaProducerController {
+
+    private final KafkaProducer kafkaProducer;
+
+    public KafkaProducerController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> publish(@RequestBody String message){
+        kafkaProducer.sendMessage(message);
+        return ResponseEntity.ok("Message sent to kafka topic");
+    }
+}
